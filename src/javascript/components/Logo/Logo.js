1d70bc7026b1db1439d09f58503eb4cdbeb7535f
journey-ji <journeyji95@gmail.com>
2023-05-01 20:54:27 +0900

    웹접근성,SEO고려한 마크업 및 웹 메인 색상 및 로고 변경

--- src/javascript/components/Logo/Logo.js
+++ src/javascript/components/Logo/Logo.js
@@ -1,17 +1,16 @@
 import { Component } from "../../core";
-import logoImg from "../../../assets/test.png";
-import './Logo.css'
+import logoImg from "../../../assets/Logo-moma.png";
+import "./Logo.css";
 class Logo extends Component {
   render() {
     const logo = document.createElement("img");
-    logo.setAttribute('class','logo-img')
+    logo.setAttribute("class", "logo-img");
     logo.setAttribute("src", logoImg);
-    logo.setAttribute('alt','웹페이지 로고')
+    logo.setAttribute("alt", "웹페이지 로고");
     logo.style.display = "block";
     logo.style.width = this.props.width;
     logo.style.height = this.props.height;
     logo.style.margin = this.props.margin;
-    
 
     return logo;
   }
